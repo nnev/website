82fa7e02c4469e1884d62aaf29931e186c242535
Axel Wagner <mail@merovius.de>
2013-12-01 12:21:09 +0100

    Lade Termine aus Datenbank

--- _plugins/aktuelles.rb
+++ _plugins/aktuelles.rb
@@ -1,29 +1,23 @@
 # encoding: utf-8
 
+require "pg"
+require "pp"
+
 module Jekyll
 	class Aktuelles < Jekyll::Generator
 		def generate(site)
-			site.pages.each do |page|
+			conn = PGconn.open(:dbname => 'nnev')
+			res = conn.exec('SELECT stammtisch, override, location, termine.date AS date, topic, abstract FROM termine LEFT JOIN vortraege ON termine.vortrag = vortraege.id ORDER BY termine.date LIMIT 4')
+			termine = []
+			res.each do |tuple|
+				tuple['stammtisch'] = (tuple['stammtisch'] == 't')
+				termine << tuple
+			end
 
-				page.data['termine'] = [
-					{
-						"date" => "2013-12-26",
-						"stammtisch" => false,
-						"topic" => "FÄLLT AUS (30c3)"
-					}, {
-						"date" => "2013-12-19",
-						"stammtisch" => false,
-						"topic" => "Penisverlängerungen"
-					}, {
-						"date" => "2013-12-12",
-						"stammtisch" => false,
-						"topic" => "This stock is about to go through the roof!"
-					}, {
-						"date" => "2012-12-05",
-						"stammtisch" => true,
-						"location" => "Mister Wu"
-					}
-				]
+			pp termine
+
+			site.pages.each do |page|
+				page.data['termine'] = termine
 			end
 		end
 	end
