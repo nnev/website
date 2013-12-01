a5318ad8851ab7aa4ca46c1edc6c9899119f62b1
Stefan Breunig <stefan-github@yrden.de>
2013-12-01 14:02:58 +0100

    wrapper für psql stuff

--- _plugins/aktuelles.rb
+++ _plugins/aktuelles.rb
@@ -6,6 +6,17 @@ require "pp"
 module Jekyll
 	class Aktuelles < Jekyll::Generator
 		def generate(site)
+			begin
+				real(site)
+			rescue => e
+				warn "\n\nAktuelles ist kaputt. Fehlermeldung:"
+				warn e.message
+				warn e.backtrace.map{|x| "\t#{x}"}.join("\n")
+				warn "\n\n"
+			end
+		end
+
+		def real(site)
 			conn = PGconn.open(:dbname => 'nnev')
 			res = conn.exec('SELECT stammtisch, override, location, termine.date AS date, topic, abstract FROM termine LEFT JOIN vortraege ON termine.vortrag = vortraege.id ORDER BY termine.date LIMIT 4')
 			termine = []
